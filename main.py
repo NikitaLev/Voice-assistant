ff9c4fdf274e07bd169b00fbd1571ec909b3eb07
MikitaLev <mikita.leventsyev@mail-cyber.com>
2022-05-28 14:57:33 +0300

    Test commit 2

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 # This is a sample Python script.
-
+import test
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from test import print_test
 
 
 def print_hi(name):
@@ -12,5 +13,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('Test')
+    print(print_test("string"))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ test.py
@@ -0,0 +1,3 @@
+def print_test(text):
+    # Use a breakpoint in the code line below to debug your script.
+    print(text + "1")
